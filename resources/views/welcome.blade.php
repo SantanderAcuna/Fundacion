c6333470f943da9027f3a992d6e477c147b3b07a
Santander Acuña Polo <panson@misena.edu.co>
2020-07-22 16:54:57 -0500

    8 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 
+<div class="container">
+  <div class="row px-5 ml-5">
+    <div class="ml-5 col-8 ">
 
+      @include('cliente.index')
 
-
-
+    </div>
+  </div>
+</div>
 
 @endsection
