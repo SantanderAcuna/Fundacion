73a0c0cb24bb931c11a09ba4712bebc63d9e46de
Santander Acuña Polo <panson@misena.edu.co>
2020-07-22 15:58:39 -0500

    cuarto commit

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ Route::get('/usuarios', 'RegisterController@create')->name('registro');
 
 // Rutas cliente
 
-Route::get('cliente', 'ClienteController@index')->name('cliente.index');
-Route::post('cliente', 'ClienteController@store')->name('cliente.guardar');
+Route::get('/', 'ClienteController@index')->name('cliente.index');
+Route::post('/', 'ClienteController@store')->name('cliente.guardar');
 
 // Ruta administrador
 
